46f60cb695e69b09c6d955168895983dcc567cab
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-03 15:43:28 +0200

    promplot version v0.16.0

--- Formula/promplot.rb
+++ Formula/promplot.rb
@@ -1,11 +1,15 @@
 class Promplot < Formula
   desc "Create plots from Prometheus metrics and send them to you"
   homepage "https://qvl.io/promplot"
-  url "https://github.com/qvl/promplot/releases/download/v0.15/promplot_0.15_mac_64bit.tar.gz"
-  version "0.15"
-  sha256 "b7a8c59f872edcd5302b6811d881282aaea3ae8660f41594dfb8135ea7fb89de"
+  url "https://github.com/qvl/promplot/releases/download/v0.16.0/promplot_0.16.0_mac_64bit.tar.gz"
+  version "0.16.0"
+  sha256 "ea2d5e10fb80108d677a573d7a1462826e4c67ce3c071ce7a51df7e5b4f46184"
 
   def install
     bin.install "promplot"
+  end
+
+  test do
+    
   end
 end
