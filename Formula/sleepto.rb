e31998118ef8978f3bdbbe0d5cbda554e828991f
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-03 15:44:00 +0200

    sleepto version v1.7.1

--- Formula/sleepto.rb
+++ Formula/sleepto.rb
@@ -1,11 +1,15 @@
 class Sleepto < Formula
   desc "Alternative to traditional task schedulers"
   homepage "https://qvl.io/sleepto"
-  url "https://github.com/qvl/sleepto/releases/download/v1.7/sleepto_1.7_mac_64bit.tar.gz"
-  version "1.7"
-  sha256 "b5db10052b486f6ffb6e8aa003af4fd1736c49586790d536ac07513d3d4fdbb6"
+  url "https://github.com/qvl/sleepto/releases/download/v1.7.1/sleepto_1.7.1_mac_64bit.tar.gz"
+  version "1.7.1"
+  sha256 "8e6c870923d60a3e7d226fbaa30ebe1854e657e9a8aa44a702f25b70e1252149"
 
   def install
     bin.install "sleepto"
+  end
+
+  test do
+    
   end
 end
