df87147c1a11115d62808ee5645dcd6558435e38
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-07 22:08:04 +0200

    httpsyet version v0.1.2

--- Formula/httpsyet.rb
+++ Formula/httpsyet.rb
@@ -1,9 +1,9 @@
 class Httpsyet < Formula
   desc "Find links you can update to HTTPS"
   homepage "https://qvl.io/httpsyet"
-  url "https://github.com/qvl/httpsyet/releases/download/v0.1.0/httpsyet_0.1.0_mac_64bit.tar.gz"
-  version "0.1.0"
-  sha256 "694aea8cbeb651e7ef3dac1c62d22acf65a1e8b4a1fee5e49653ce74d6f0c60b"
+  url "https://github.com/qvl/httpsyet/releases/download/v0.1.2/httpsyet_0.1.2_mac_64bit.tar.gz"
+  version "0.1.2"
+  sha256 "bc2cbb28a655c8178c5703b1287618a379a18a6f9f34c22ecfc01285896c966c"
 
   def install
     bin.install "httpsyet"
