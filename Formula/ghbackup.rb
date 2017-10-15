750eee670ec7de5d8ecc3741665fbba423cdb21c
goreleaserbot <goreleaser@carlosbecker.com>
2017-10-15 14:57:57 +0200

    ghbackup version v1.11.3

--- Formula/ghbackup.rb
+++ Formula/ghbackup.rb
@@ -1,9 +1,9 @@
 class Ghbackup < Formula
   desc "Embarrassingly simple GitHub backup tool"
   homepage "https://qvl.io/ghbackup"
-  url "https://github.com/qvl/ghbackup/releases/download/v0.1.11.3/ghbackup_0.1.11.3_mac_64bit.tar.gz"
-  version "0.1.11.3"
-  sha256 "028af7e05b26bd664bc4a4b21117ddbf8282f78481f459e23575d046f23d881c"
+  url "https://github.com/qvl/ghbackup/releases/download/v1.11.3/ghbackup_1.11.3_mac_64bit.tar.gz"
+  version "1.11.3"
+  sha256 "5c819cfaa402e7aa8b0287a6132261bc227cbf0bf34589e5d4e9dcefca5b8125"
 
   def install
     bin.install "ghbackup"
