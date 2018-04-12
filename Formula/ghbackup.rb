dad7e4118d8d571c5d1bfb394a212cbed5b65bf8
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-12 20:56:35 +0200

    Brew formula update for ghbackup version v1.12.0

--- Formula/ghbackup.rb
+++ Formula/ghbackup.rb
@@ -1,15 +1,11 @@
 class Ghbackup < Formula
   desc "Embarrassingly simple GitHub backup tool"
   homepage "https://qvl.io/ghbackup"
-  url "https://github.com/qvl/ghbackup/releases/download/v1.11.3/ghbackup_1.11.3_mac_64bit.tar.gz"
-  version "1.11.3"
-  sha256 "5c819cfaa402e7aa8b0287a6132261bc227cbf0bf34589e5d4e9dcefca5b8125"
+  url "https://github.com/qvl/ghbackup/releases/download/v1.12.0/ghbackup_1.12.0_mac_64bit.tar.gz"
+  version "1.12.0"
+  sha256 "a1a415948a66b8c35b727b0b30ef657fbbb4820ca7912448166f295204f370f3"
 
   def install
     bin.install "ghbackup"
-  end
-
-  test do
-    
   end
 end
